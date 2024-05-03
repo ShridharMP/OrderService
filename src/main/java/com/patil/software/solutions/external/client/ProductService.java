1f2cc39ffb14edcd92f6d5e6bf1cba4e5dcaf028
ShridharMP <mpshridhar2016@gmail.com>
2024-05-03 10:12:39 +0530

    circuit breaker

--- src/main/java/com/patil/software/solutions/external/client/ProductService.java
+++ src/main/java/com/patil/software/solutions/external/client/ProductService.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.patil.software.solutions.external.client.exception.CustomException;
+
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
 	@PutMapping("/reduceQuantity/{id}")
 	ResponseEntity<Void> reduceProductQuantity(@PathVariable("id") long productId, @RequestParam long quantity);
+
+	default void myFallBackMethod(Exception e) {
+		throw new CustomException("Product service is not availabe", "UNAVAILABLE", 500);
+	}
 }
